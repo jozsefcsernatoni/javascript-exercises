b9f353c31a07fcb6769794854568dd4a14dd7764
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-08 14:45:03 +0200

    Completed exercise 09
    The task was to sum up positive integers between to numbers, received as input

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(nrOne,nrTwo) {
+if (typeof(nrOne)!="number" || typeof(nrTwo)!="number" || nrOne<0 || nrTwo<0 || !Number.isInteger(nrOne) || !Number.isInteger(nrOne)){
+    return 'ERROR';
+} else {
+    if (nrOne>nrTwo){
+        let temp=nrOne;
+        nrOne=nrTwo;
+        nrTwo=temp;
+    }
+let sum=0;
+for(let i=nrOne;i<=nrTwo;i++){
+sum+=i;
+}
+return sum;
+}
 };
+
+console.log(sumAll(1, 4)); // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
