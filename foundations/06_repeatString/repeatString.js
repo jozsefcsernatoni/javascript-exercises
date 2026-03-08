265f26f10c8ece0c23c63113b048942541ad632f
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-08 07:53:11 +0200

    Completed repeat string exercise
    Created a callback function that repeats a string a specified number of times, and displays error message if the provided number is negative.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str,times) {
+let fullText="";
+if (times>=0){
+    for(let i=0;i<times;i++){
+    fullText+=str;
+    }
+}
+ else {
+    fullText='ERROR';
+}
+return fullText;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
