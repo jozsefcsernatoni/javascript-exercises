6ad0e6d7b6a2b989a5f877efa9e2bf5050f22c34
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-09 06:51:26 +0200

    Completed exercise nr 10
    The task was to build a leapYear calculator, using if

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(usrYear) {
+if(!(usrYear % 4) && (usrYear % 100)){
+    return true;
+} else if ( !(usrYear % 400)){
+    return true;
+} else {
+    return false;
+}
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
