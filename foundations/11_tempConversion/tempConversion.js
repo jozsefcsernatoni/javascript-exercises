54130902cdec597a1fd30828e90fab9cdb73a247
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-09 07:18:42 +0200

    Completed exercise 11
    The purpose was to create a celsius to fahrenheit and the fahrenheit to celsius calculator, which returns a 1 decimal number. The task required self study to identify the right function (toFixed) and to convert back the result to number, as the function returns a string

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fValue) {
+cResult=(fValue-32)*5/9;
+return Number(cResult.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cValue) {
+fResult=(cValue*9/5+32);
+return Number(fResult.toFixed(1));
 };
 
 // Do not edit below this line
