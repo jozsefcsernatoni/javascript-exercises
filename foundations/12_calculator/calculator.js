690cb5ecb8986b094a02979af98b780aa3b25aba
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-23 07:51:51 +0200

    Completed
    Basic arithmetic operations, including application of reduce function on array to compute the sum and product of array elements.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1,number2) {
+	return number1-number2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    const init=0;
+    return  arr.reduce((acc,curr)=>acc+curr,init,
+);};
+
+const multiply = function(arr) {
+    
+    return  arr.reduce((acc,curr)=>acc * curr , 
+);
 };
 
-const multiply = function() {
-
+const power = function(number1,number2) {
+return number1**number2;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number1) {
+if(!number1==0){
+  let fact=1;
+  for(let i=1;i<=number1;i++){
+    fact*=i;
+  }
+  return fact;
+}
+else return 1;
 };
 
 // Do not edit below this line
