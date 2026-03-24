69ad60a4c9d957ebc78fa965e2cb92f9c8f6a653
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-24 07:01:04 +0200

    Completed exercise 14
    The function created an array with the fibonacci number and returns the required element. The function works with strings, and does not accept negative values

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(usrNumber) {
+usrNumber=Number(usrNumber);
+if(usrNumber>=0){
+const fiboArr=[0,1]; //initial values for the sequence
 
-};
+function createFibo(myNumber){
+    if(myNumber=>2){
+        for(let i=2;i<=myNumber;i++){
+            fiboArr[i]=fiboArr[i-2]+fiboArr[i-1];
+        }
+    }
+}
+createFibo(usrNumber);
+return fiboArr[usrNumber];
+}else return "OOPS"
+} ;
+
 
 // Do not edit below this line
 module.exports = fibonacci;
