2305f5820d0a1d52b15f2906a9ccb213c51a8f82
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-24 06:19:18 +0200

    Completed exercise 13
    The function transforms the text to lower case, splits it into an array, removes punctuation marks, and compares the result with the reverse of the array.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+str=str.toLowerCase();
 
+//filters out punctuation marks
+const strArr=str.split("").filter(char=>{
+    return /[a-zA-Z0-9]/.test(char);
+});
+const strArrReverse=[...strArr].reverse();
+if(JSON.stringify(strArr)==JSON.stringify(strArrReverse)){
+    return true;
+} else return false;
 };
 
+console.log(palindromes('Racecar!'));
 // Do not edit below this line
 module.exports = palindromes;
