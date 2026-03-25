dd3b07d2b9d9564fd440210e115fa62e2b80dc73
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-25 07:08:04 +0200

    Completed exercise 15
    Used reduce to create an array, containing one property of the objects, from an array of objects.

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr){
+    const result = arr.reduce(function(acc,item){
+        if(!acc.includes(item.title)){
+            acc.push(item.title);
+        }
+        return acc;
+    },[])
+    return result;
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
