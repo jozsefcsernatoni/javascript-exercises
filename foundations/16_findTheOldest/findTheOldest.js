a45d7b677ecc941dee2a3da3936248c7e26ca7c2
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-27 03:48:08 +0200

    Completed exercise 16
    The function creates a shallow copy of the array, computes a new property based on 1 or 2 existing properties and displayes the object with the highest value.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest =  ((usrArr)=>{
+  const newArr=[...usrArr];
+  const result= newArr
+  .map((item)=>{
+    let age;
+    if (item.hasOwnProperty("yearOfDeath")){
+        age=Number(item.yearOfDeath)-Number(item.yearOfBirth);
+    } else {
+        age=new Date().getFullYear()-Number(item.yearOfBirth);
+    }
+    if(! newArr.includes(item.age))
+    {
+       item.age=age;
+    }
+    return item;
+  },{})
+  .reduce(function(prev, curr){
+    return prev.age>curr.age ? prev: curr;
+    },0)
+    
+  
+  return result;
+});
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
